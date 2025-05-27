c2d626396234f1e256b36b1ec1dd13a5dd3f32fb
s.babayan <s.babayan@notamedia.ru>
2025-05-27 13:47:27 +0400

    fix svg

--- src/styles/icons/icon.tsx
+++ src/styles/icons/icon.tsx
@@ -1,14 +1,14 @@
 export const ArrowLeftIcon = () => {
-    return (<svg width="10" height="14" viewBox="0 0 10 14" fill="none">
-        <path d="M8.5 1L2 7L8.5 13" strokeWidth="2" />
+    return (<svg width="10" height="14" viewBox="0 0 10 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M8.49988 0.750001L2.24988 7L8.49988 13.25" stroke-width="2"/>
     </svg>
     )
 }
 
 
 export const ArrowRightIcon = () => {
-    return (<svg width="10" height="14" viewBox="0 0 10 14" fill="none">
-        <path d="M1.5 1L8 7L1.5 13" strokeWidth="2" />
-    </svg>
+    return (<svg width="10" height="14" viewBox="0 0 10 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M1.50012 0.750001L7.75012 7L1.50012 13.25" stroke-width="2"/>
+    </svg>    
     )
 }
